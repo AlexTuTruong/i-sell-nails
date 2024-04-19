0edab73f72a02f38903f85738de0b232cf68341a
Alex Tu <alextutruong@gmail.com>
2024-04-18 20:30:54 -0400

    Removed unnecessary numpy import

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-import numpy
 from . import get_db, models
 
 router = APIRouter()
